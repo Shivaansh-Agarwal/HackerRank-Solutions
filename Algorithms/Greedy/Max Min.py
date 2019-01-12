3cfcb1e53ac0a39b74b0b35e1a99edd5ab0173e8
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2019-01-13 01:53:01 +0530

    Create Max Min.py

---
+++ Algorithms/Greedy/Max Min.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maxMin function below.
+def maxMin(k, arr):
+    arr.sort()
+    n = len(arr)
+    min1 = arr[0]
+    max1 = arr[k-1]
+    unfairness = max1 - min1
+    for i in range(1,n-k+1):
+        min1 = arr[i]
+        max1 = arr[i+k-1]
+        if(unfairness>max1 - min1):
+            unfairness = max1-min1
+    return unfairness
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    k = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
